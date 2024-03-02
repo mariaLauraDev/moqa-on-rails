928563fcd0a2b0ae20fbb8d7f39bcb433102c3df
@laura.alves <laura.alves@gocase.com.br>
2024-03-02 16:29:30 -0300

    feat: add mongoid configuration

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,12 @@ module MoqaOnRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.orm :mongoid
+    end
+
+    config.time_zone = ActiveSupport::TimeZone.new('America/Fortaleza')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
