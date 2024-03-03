62c94aef9f0e99aa8cf86a1e04ee5cf9dc63ab81
@laura.alves <laura.alves@gocase.com.br>
2024-03-03 00:14:43 -0300

    feat: view all toutes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @routes = Rails.application.routes.routes.map do |route|
+      { path: route.path.spec.to_s, controller: route.defaults[:controller], action: route.defaults[:action] }
+    end.reject { |route| route[:path].starts_with?("/rails") } # Filtrar rotas internas do Rails
   end
 end
